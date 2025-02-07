e16845c028e3c9fbcea27b267811aeaf7bd63ae5
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-02-07 12:52:38 +0700

    update

--- resources/views/admin/post/local-admin-promotion/local-admin-promotion.blade.php
+++ resources/views/admin/post/local-admin-promotion/local-admin-promotion.blade.php
@@ -149,4 +149,9 @@
 </div>
 @endforeach
 
+<script src="{{asset('js/datatable.js')}}"></script>
+
 @endsection
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js" defer></script>
